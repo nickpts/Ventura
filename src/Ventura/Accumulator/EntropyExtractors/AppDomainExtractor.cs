3eb6e52760715863f9516411935b36079ae1d51e
Nick <nicholas.patsaris@gmail.com>
2018-05-22 19:42:04 +0100

    Adding application domain extractor

---
+++ src/Ventura/Accumulator/EntropyExtractors/AppDomainExtractor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using Ventura.Interfaces;
+
+namespace Ventura.Accumulator.EntropyExtractors
+{
+    public class AppDomainExtractor: EntropyExtractorBase, IEntropyExtractor
+    {
+        public AppDomainExtractor(int sourceNumber) : base(sourceNumber)
+        {
+        }
+
+        public override string SourceName { get; protected set; } = ".NET Application domain entropy extractor";
+
+        protected override Task<byte[]> ExtractEntropicData()
+        {
+            byte[] extraction()
+            {
+                AppDomain.MonitoringIsEnabled = true;
+
+                var domain = AppDomain.CurrentDomain;
+                
+                var survivedMemory = domain.MonitoringSurvivedMemorySize;
+                var totalMemory = domain.MonitoringTotalAllocatedMemorySize;
+                var totalProcessorTime = domain.MonitoringTotalProcessorTime;
+
+                return BitConverter.GetBytes(survivedMemory + totalMemory + totalProcessorTime.Ticks);
+            }
+
+            return Task.Run((Func<byte[]>)extraction);
+        }
+    }
+}
