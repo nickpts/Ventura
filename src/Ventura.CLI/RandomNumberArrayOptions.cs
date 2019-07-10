3157aecda67b04c3ec1a7a0cb0d2dbe4aec5e695
Nick <nicholas.patsaris@gmail.com>
2019-07-10 12:34:09 +0100

    Revert "moved dockerfile, deleted duplicate directory"
    This reverts commit 577494fdac808b26546a0d2feaa73558a502c9a9.

---
+++ src/Ventura.CLI/RandomNumberArrayOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace Ventura.Cli
+{
+	[Verb("rns", HelpText = "Generates a sequence of random numbers")]
+	public class RandomNumberArrayOptions: Options
+	{
+		[Option('n', "length", Required = true, HelpText = "How many random numbers to generate")]
+		public int ArrayLength { get; set; }
+	}
+}
